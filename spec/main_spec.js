e39e04974deebab2211496dfbbb62f816ed7ae35
e <1456958517@qq.com>
2017-08-17 16:49:33 +0800

    20minutes

--- spec/main_spec.js
+++ spec/main_spec.js
@@ -12,20 +12,13 @@ var main = require("../lib/main.js");
 describe("测试描述", function(){
     sinon.spy(console, 'log');
 
-    it("测试用例1", function(){
+    it("90 change to 2335", function(){
 
-        var result = main();
-        var expect_string = '';
+        var result = main(90);
+        var expect_string = '2335';
         
         expect(expect_string).to.equal(result);
     });
 
-    it("测试用例2", function(){
 
-        main();
-        var result = _.flatten(console.log.args).join("\n");
-        var expect_string = '';
-
-        expect(expect_string).to.equal(result);
-    });
 });
